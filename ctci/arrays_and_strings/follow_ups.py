96a6293e18a2bc8d222844dd6cd25097ceaa64ba
timha <dev@timha.io>
2022-02-20 04:59:20 -0800

    add follow_ups.py

---
+++ ctci/arrays_and_strings/follow_ups.py
@@ -0,0 +1,8 @@
+"""
+Follow up on
+    - bit vector
+    - ascii, extended ascii, unicode
+        ord(), chr()
+    - python strings are immutable. what's the fastest way to modify them? work in arrays?
+    - sorted time and space complexities
+"""
