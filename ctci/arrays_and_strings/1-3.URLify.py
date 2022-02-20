d5e6ce41d461e19605e3dff8133b1f549e669476
timha <dev@timha.io>
2022-02-20 05:59:50 -0800

    fix range parameter

--- ctci/arrays_and_strings/1-3.URLify.py
+++ ctci/arrays_and_strings/1-3.URLify.py
@@ -5,7 +5,6 @@ assume the string has sufficient space at the end to hold the additional charact
 
 """
 Initial thoughts
-    work in array of chars
     one whitespace seems simple, what if there are 2+ spaces in a row?
         how do we keep track of all the characters that are being replaced?
         could use temp array but defeats the purpose of doing it in place
@@ -16,7 +15,7 @@ Initial thoughts
 
 def URLify(string, length):
     overwrite_index = len(string)
-    for i in range(length - 1, 0, -1):
+    for i in range(length - 1, -1, -1):
         if string[i] == ' ':
             string[overwrite_index - 3:overwrite_index] = ['%', '2', '0']
             overwrite_index -= 3
@@ -24,11 +23,10 @@ def URLify(string, length):
             overwrite_index -= 1
             string[overwrite_index] = string[i]
 
-
 my_test = list('Mr John Smith    ')
-my_test2 = list('sd  f       ')
+my_test2 = list('   s d  f               ')
 URLify(my_test, 13)
-URLify(my_test2, 6)
+URLify(my_test2, 10)
 print(''.join(my_test))
 print(''.join(my_test2))
 
@@ -36,6 +34,15 @@ print(''.join(my_test2))
 Time: O(n)
 Space: O(n)
 solution is a little bit unreadable
-we could abstract the solution so whitespaces can be replaced by anything
+    the 3's seem hardcoded but are just related to '%20'
+we could abstract the solution so whitespaces can be replaced by anything and less hardcoding
     this assumes the length is true length
+"""
+
+
+"""
+Learnings
+1. look at the problem from all angles, don't be satisfied with first thing that comes to mind
+    e.g. front -> back, back -> front, etc
+2. range(start, stop (exclusive), step)
 """
