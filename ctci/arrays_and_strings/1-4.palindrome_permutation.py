cb1894406892f33b2bd2e7187d65a0b9cf7ec530
timha <dev@timha.io>
2022-02-20 06:16:45 -0800

    add palindrome_permutation.py

---
+++ ctci/arrays_and_strings/1-4.palindrome_permutation.py
@@ -0,0 +1,37 @@
+"""
+given a string, check if it is a permutation of a palindrome
+ignore casing and non-letter characters
+"""
+
+"""
+Initial thoughts
+    eliminate pairs and passes if <= 1 character remains
+        racecar -> aaccerr -> e (acceptable)
+        racecarf -> aaccefrr -> ef (unacceptable)
+    can we take advantage of the fact that we're only using lowercase letters, a-z?
+        could use array of size 26 and store counts of ascii values
+        correction to earlier thought, we don't care about eliminating pairs, we just need to check how many values in our array
+            are odd. # odd values in array <= 1
+        lowercase ASCII: 97-122
+        ord(char) - 97
+    can we do this without additional space?
+        i don't think we can without greatly sacrificing time 
+    does sorting help us? not really
+    lowercase and strip whitespace
+"""
+
+def palindrome_permutation(string):
+    str_arr = list(string.lower())
+    letter_counts = [0] * 26
+    for letter in str_arr:
+        ascii_val = ord(letter) - 97
+        if ascii_val >= 0 and ascii_val <= 26:
+            letter_counts[ascii_val] += 1
+    
+    num_odd = 0
+    for val in letter_counts:
+        num_odd += val % 2
+    return num_odd <= 1
+
+print(palindrome_permutation("tomcat"))
+print(palindrome_permutation("race     CAr"))
